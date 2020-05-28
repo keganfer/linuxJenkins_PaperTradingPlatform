1969f3e536d5212988fab65e8ea5910eafa619c4
Kegan Ferreira <keganferreira@outlook.com>
2020-05-28 16:14:53 +1000

    changed packaging to .war

--- src/main/java/com/fdm/PaperTradingPlatform/PaperTradingPlatformApplication.java
+++ src/main/java/com/fdm/PaperTradingPlatform/PaperTradingPlatformApplication.java
@@ -3,6 +3,8 @@ package com.fdm.PaperTradingPlatform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -11,8 +13,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = {"com.fdm.controllers","com.fdm.setup","com.fdm.service"})
 @EntityScan(basePackages = {"com.fdm.model"})
 @EnableJpaRepositories(basePackages = {"com.fdm.dal"})
-public class PaperTradingPlatformApplication {
+public class PaperTradingPlatformApplication extends SpringBootServletInitializer {
 
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(PaperTradingPlatformApplication.class);
+	    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PaperTradingPlatformApplication.class, args);
 	}
