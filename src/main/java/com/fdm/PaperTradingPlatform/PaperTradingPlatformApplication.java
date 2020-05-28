3948bfcf3e871d74c79e971cba2b4bb461de06df
Kegan Ferreira <keganferreira@outlook.com>
2020-05-28 16:26:17 +1000

    changed packaging back to .jar packaging

--- src/main/java/com/fdm/PaperTradingPlatform/PaperTradingPlatformApplication.java
+++ src/main/java/com/fdm/PaperTradingPlatform/PaperTradingPlatformApplication.java
@@ -3,8 +3,7 @@ package com.fdm.PaperTradingPlatform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -13,13 +12,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = {"com.fdm.controllers","com.fdm.setup","com.fdm.service"})
 @EntityScan(basePackages = {"com.fdm.model"})
 @EnableJpaRepositories(basePackages = {"com.fdm.dal"})
-public class PaperTradingPlatformApplication extends SpringBootServletInitializer {
+public class PaperTradingPlatformApplication  {
 
-	
-	 @Override
-	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	        return application.sources(PaperTradingPlatformApplication.class);
-	    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PaperTradingPlatformApplication.class, args);
